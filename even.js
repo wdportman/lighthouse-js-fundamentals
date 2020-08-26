adaf06165563ea1d215191d869e5e7595b89d24c
Will Portman <wdportman@gmail.com>
2020-08-26 19:11:17 +0000

    Function to test whether numbers are even or odd

---
+++ even.js
@@ -0,0 +1,9 @@
+let isEven = function(num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log("tenIsEven: "+isEven(10));
+console.log("elevenIsEven: "+isEven(11));
