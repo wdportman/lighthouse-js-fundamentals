07332d1e18acfcc45cd9530b8502c1d92c3ea13d
Will Portman <wdportman@gmail.com>
2020-08-27 23:17:16 +0000

    Using a while loop, a for-of loop, and a regular for-loop with an array

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+
+for (let ingredient of ingredients) {
+  console.log(ingredient);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+for (let i = ingredients.length; i >=0; i--) {
+  console.log(ingredients[i]);
+}
